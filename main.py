f1e0f0c2ecbf948cc8ef346cd5a30fb433a5bf86
qurratulaini3010 <missquratulaini@gmail.com>
2023-03-25 20:44:12 +0800

    qurratul

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 @app.route('/cuba')
 def cuba():
     return render_template('cuba.html', tahun='2023')
+
+
 if __name__ == "__main__":
     app.run()
